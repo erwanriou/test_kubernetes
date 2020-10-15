3a190e47d6d171ed895f01f7be8ba9ac9a02c2df
Erwan <rr.1.riou@gmail.com>
2020-10-15 12:07:54 -0500

    [Project] - update mongoURIs

--- auth/src/services/database.js
+++ auth/src/services/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 module.exports = async () => {
   try {
-    await mongoose.connect("mongodb://auth-srv-mongo:27017/auth", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
--- tickets/src/services/database.js
+++ tickets/src/services/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 module.exports = async () => {
   try {
-    await mongoose.connect("mongodb://tickets-srv-mongo:27017/tickets", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
