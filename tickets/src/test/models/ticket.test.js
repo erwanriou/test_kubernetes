82d5c1f8ce57631af6cdfb7fb213a02d7c741197
Erwan <rr.1.riou@gmail.com>
2020-10-25 12:53:16 -0600

    [Project] - update versioning2

--- tickets/src/test/models/ticket.test.js
+++ tickets/src/test/models/ticket.test.js
@@ -25,3 +25,20 @@ it("It implement optimistic concurency control", async done => {
   }
   throw new Error("Version increment control failing")
 })
+
+it("increment the version number on multiple saves", async () => {
+  const ticket = new Ticket({
+    title: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    userId: new mongoose.Types.ObjectId()
+  })
+
+  await ticket.save()
+  expect(ticket.__v).toEqual(0)
+
+  await ticket.save()
+  expect(ticket.__v).toEqual(1)
+
+  await ticket.save()
+  expect(ticket.__v).toEqual(2)
+})
