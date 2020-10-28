9cb44a09990e95984bea1c930cda39f0c4412199
Erwan <rr.1.riou@gmail.com>
2020-10-28 14:45:59 -0600

    [Project] - fix issue

--- expiration/src/services/natsStreaming.js
+++ expiration/src/services/natsStreaming.js
@@ -2,6 +2,15 @@ const { NatsWrapper } = require("./natsWrapper")
 const { OrderCreatedList } = require("../events/listeners/orderCreatedList")
 
 module.exports = async natsStreaming => {
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined")
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined")
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined")
+  }
   // CONNECT NATS
   await NatsWrapper.connect(
     process.env.NATS_CLUSTER_ID,
--- orders/src/services/natsStreaming.js
+++ orders/src/services/natsStreaming.js
@@ -3,6 +3,15 @@ const { TicketCreatedList } = require("../events/listeners/ticketCreatedList")
 const { TicketUpdatedList } = require("../events/listeners/ticketUpdatedList")
 
 module.exports = async natsStreaming => {
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined")
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined")
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined")
+  }
   // CONNECT NATS
   await NatsWrapper.connect(
     process.env.NATS_CLUSTER_ID,
