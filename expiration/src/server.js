4346f05f82ce52080293c93fee9e31f9f758d8b6
Erwan <rr.1.riou@gmail.com>
2020-10-27 19:36:22 -0600

    [Project] - update expiration service2

--- expiration/src/server.js
+++ expiration/src/server.js
@@ -1,10 +1,4 @@
-const app = require("./app")
 const transaction = require("./services/transactions")
 
 // CONNECT DATABASE
 transaction("Ticket")
-
-// LISTEN APP
-app.listen(3000, () => {
-  console.log("listening on port 3000!")
-})
